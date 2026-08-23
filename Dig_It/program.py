66f5f52105018f36cc73af68d26fe7bd8c60be58
pz119 <156856562+pz119@users.noreply.github.com>
2026-08-23 10:23:17 -0400

    Update program.py

--- Dig_It/program.py
+++ Dig_It/program.py
@@ -414,6 +414,7 @@ async def ask():
     print("You dodged the health decrease.")
   stimHealth = health
   inp = await input_async("Would you like to dig, explore, scavenge, escape, or eat?")
+    inp = inp.lower()
   if inp == "dig":
     await dig()
     return
